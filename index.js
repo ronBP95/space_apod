fb742f17833589afba76d42a9b8cb292e58855dd
ronbp <ronmagpantay9513@gmail.com>
2022-02-09 23:59:08 -0800

    Issue found with app.use. It can only be used for particular routes which is why my get route works in postman but not the post. Created a separate route in index.js for the post route for the meanwhile

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import dayjs from "dayjs"
 import router from "./routes/api/members.js";
 
 const app = Express();
-const express = Express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
@@ -18,8 +17,8 @@ const logger = (req, res, next) => {
 app.use(logger);
 
 // Body Parser Middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(Express.json());
+app.use(Express.urlencoded({extended: false}));
 
 // Set static folder
 app.use(Express.static(path.join(__dirname, 'public')));
@@ -27,6 +26,10 @@ app.use(Express.static(path.join(__dirname, 'public')));
 // Members API Routes
 app.use('/api/members', router)
 app.use('/api/members/:id', router)
+
+app.post('/api/members', (req, res) => {
+    res.send(req.body)
+})
 
 // Listen on port
 app.listen(port, () => console.log ("listening on port:" + port))
