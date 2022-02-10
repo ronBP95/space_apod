afbf9563d8d58ab84d34a1ec153d3b12d62a0a18
ronbp <ronmagpantay9513@gmail.com>
2022-02-09 16:41:29 -0800

    installed then uninstalled moment as it is legacy content

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import Express from "express";
 import path from "path"
 import { fileURLToPath } from "url";
 import members from "./Members.js"
+import moment from "moment"
 
 const app = Express();
 const port = 3000;
@@ -9,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
 const logger = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format}`);
     next();
 }
 
