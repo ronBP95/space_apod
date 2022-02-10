e4b89a672b1a5a66a7003f9f6bd80579b3ffdef9
ronbp <ronmagpantay9513@gmail.com>
2022-02-09 16:14:12 -0800

    defined filename and dirname for app.use

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 import Express from "express";
-import Space from "./space.js";
+import path from "path"
+import { fileURLToPath } from "url";
 
 const app = Express();
 const port = 3000;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
 // GET, PUT, POST, DELETE
 
-
-// Index Route
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+// Set static folder
+app.use(Express.static(path.join(__dirname, 'public')));
 
 // Listen on port
 app.listen(port, () => console.log ("listening on port:" + port))
