21906365fc217aee06040a924d02a789a814e8e1
ronbp <ronmagpantay9513@gmail.com>
2022-02-09 16:38:11 -0800

    initialized middleware. prepping to add moment

---
+++ Members.js
@@ -0,0 +1,17 @@
+export default function members() {
+    const members = [
+        {
+            id: 1,
+            name: 'John Doe',
+            email: 'john@gmail.com',
+            status: 'active'
+        },
+        {
+            id: 2,
+            name: 'Bob Williams',
+            email: 'bob@gmail.com',
+            status: 'inactive'
+        }
+    ]
+    return members;
+}
--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 import Express from "express";
 import path from "path"
 import { fileURLToPath } from "url";
+import members from "./Members.js"
 
 const app = Express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
 
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+    next();
+}
+
+// Init Middleware
+app.use(logger);
+
 // GET, PUT, POST, DELETE
+// ROUTES
+
+// Gets All Members
+app.get ('/api/members', (req, res) => {
+    res.json(members());
+})
 
 // Set static folder
 app.use(Express.static(path.join(__dirname, 'public')));
