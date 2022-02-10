54cd614f110a3db4a6fa46768f4f71ef5b373198
ronbp <ronmagpantay9513@gmail.com>
2022-02-09 23:50:25 -0800

    post route is not working at the moment. receiving error: Cannot post at route. Will proceed with creating the frontend and linking the nasa APOD api

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import Express from "express";
+import Express, { urlencoded } from "express";
 import path from "path"
 import { fileURLToPath } from "url";
 import dayjs from "dayjs"
 import router from "./routes/api/members.js";
 
 const app = Express();
+const express = Express();
 const port = 3000;
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
@@ -16,12 +17,16 @@ const logger = (req, res, next) => {
 // Init Middleware
 app.use(logger);
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 // Set static folder
 app.use(Express.static(path.join(__dirname, 'public')));
 
 // Members API Routes
-app.use('/', router)
-app.use('/:id', router)
+app.use('/api/members', router)
+app.use('/api/members/:id', router)
 
 // Listen on port
 app.listen(port, () => console.log ("listening on port:" + port))
--- routes/api/members.js
+++ routes/api/members.js
@@ -3,12 +3,12 @@ import members from "../../Members.js"
 const router = express.Router();
 
 // Gets All Members
-router.get ('/api/members', (req, res) => {
+router.get ('/', (req, res) => {
     res.json(members);
 })
 
 // Get Single Member
-router.get('/api/members/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const found = members.some(member => member.id === parseInt(req.params.id));
     
     if (found) {
