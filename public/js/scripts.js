3b7c684ea37ffd4a1f40781e24021b7e043bbbfa
ronbp <ronmagpantay9513@gmail.com>
2022-02-16 15:44:33 -0800

    prepping to display apod data in app

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,3 +4,20 @@ const navbarLinks = document.getElementsByClassName("navbar-links")[0]
 togglebutton.addEventListener("click", () => {
     navbarLinks.classList.toggle("active")
 })
+
+// ex. query = https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY
+
+fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY", {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+})
+    .then(res => {
+            return res.json()
+    })
+    .then ( res => {
+        appendData(data);
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log("ERROR"))
